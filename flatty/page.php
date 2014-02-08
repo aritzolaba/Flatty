29f62180adf449647ba9f5830641cb13297cc8fc
Aritz Olabarrieta <fakemail@fakemail.com>
2014-02-08 22:17:32 +0100

    Version 1.1.0 [Release Candidate]
    Added fonts the correct way.
    Removed theme utilities from functions.php

--- flatty/page.php
+++ flatty/page.php
@@ -3,7 +3,7 @@ if (!defined('ABSPATH')) {echo '<h1>Forbidden</h1>'; exit();} get_header(); ?>
 
 <div class="row">
 
-    <div class="col-sm-8 col-md-8 <?php if ($flatty_theme_options['sidebar_position'] == 'left') echo 'pull-right'; ?>">
+    <div class="col-sm-8 col-md-9 <?php if ($flatty_theme_options['sidebar_position'] == 'left') echo 'pull-right'; ?>">
 
         <?php if ($flatty_theme_options['breadcrumb'] == 1) get_template_part('partials/breadcrumb'); ?>
 
@@ -29,7 +29,7 @@ if (!defined('ABSPATH')) {echo '<h1>Forbidden</h1>'; exit();} get_header(); ?>
 
     </div>
 
-    <aside class="col-sm-4 col-md-4 <?php if ($flatty_theme_options['sidebar_position'] == 'left') echo 'pull-left'; ?>">
+    <aside class="col-sm-4 col-md-3 <?php if ($flatty_theme_options['sidebar_position'] == 'left') echo 'pull-left'; ?>">
         <?php if ($flatty_theme_options['sidebar_admin'] == 1) get_template_part('partials/sidebar-admin'); ?>
         <?php get_sidebar(); ?>
     </aside>
